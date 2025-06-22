1f15e919cc91c4edebc0c384c658f5d9227d7f9f
ha_q <dandelion2110@gmail.com>
2025-06-22 15:45:15 +1000

    create maxNumber, wordsInNumFiles and their unit test

--- Practice/src/main/java/com/dtkh/Java/Practice/WordsInFiles.java
+++ Practice/src/main/java/com/dtkh/Java/Practice/WordsInFiles.java
@@ -3,7 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
+
+import edu.duke.DirectoryResource;
 
 public class WordsInFiles {
 	private HashMap<String, ArrayList<String>> wordsMap;
@@ -16,7 +19,7 @@ public class WordsInFiles {
 		wordsMap.forEach((k, v) -> {System.out.printf("'%s' found in files of %s\n", k, v.toString());});
 	}
 	
-	public void addWordsFromFile(File file) throws FileNotFoundException {
+	private void addWordsFromFile(File file) throws FileNotFoundException {
 		String fileName = file.getName();
 		Scanner scanner = new Scanner(file);
 		while(scanner.hasNextLine()) {
@@ -36,5 +39,33 @@ public class WordsInFiles {
 		}
 		scanner.close();
 	}
+	public void buildWordFileMap(File directory) throws FileNotFoundException{
+		wordsMap.clear();
+		if(directory.isDirectory()) {
+			for(File file : directory.listFiles()) {
+				addWordsFromFile(file);
+			}
+		}
+	}
 
+	public int maxNumber() {
+		int max=0;
+		for(Entry<String, ArrayList<String>> entry: wordsMap.entrySet()) {
+			int count = entry.getValue().size();
+			max = count > max ? count : max;
+		}
+		return max;
+	}
+	
+	public ArrayList<String> wordsInNumFiles(int number){
+		ArrayList<String> resultArrayList = new ArrayList<String>();
+		int count = 0;
+		for(Entry<String, ArrayList<String>> entry: wordsMap.entrySet()) {
+			count = entry.getValue().size();
+			if (count == number) {
+				resultArrayList.add(entry.getKey());
+			}
+		}
+		return resultArrayList;
+	}
 }
--- Practice/src/test/java/com/dtkh/Java/Practice/WordsInFilesTest.java
+++ Practice/src/test/java/com/dtkh/Java/Practice/WordsInFilesTest.java
@@ -1,8 +1,12 @@
 package com.dtkh.Java.Practice;
 
+import static org.junit.Assert.*;
+
 import java.io.File;
 
 import org.junit.Test;
+
+import edu.duke.DirectoryResource;
 
 public class WordsInFilesTest {
 	private WordsInFiles wordsInFiles;
@@ -11,14 +15,51 @@ public class WordsInFilesTest {
 		wordsInFiles = new WordsInFiles();
 	}
 
+//	@Test
+//	public void addWordsFromFileTest() {
+//		try {
+//			wordsInFiles.addWordsFromFile(new File("src/test/java/smallhamlet.txt"));
+//			wordsInFiles.displayWordsMap();
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+//
+//	}
+	
 	@Test
-	public void addWordsFromFileTest() {
+	public void buildWordFileMapTest() {
 		try {
-			wordsInFiles.addWordsFromFile(new File("src/test/java/smallhamlet.txt"));
+			wordsInFiles.buildWordFileMap(new File("src/test/java/test"));
 			wordsInFiles.displayWordsMap();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-
+	}
+	
+	@Test
+	public void maxNumberTest() {
+		try {
+			wordsInFiles.buildWordFileMap(new File("src/test/java/test"));
+			wordsInFiles.displayWordsMap();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			int result = wordsInFiles.maxNumber();
+			assertEquals(1, result);
+		}
+	}
+	
+	@Test
+	public void wordsInNumFilesTest() {
+		try {
+			wordsInFiles.buildWordFileMap(new File("src/test/java/test"));
+			wordsInFiles.displayWordsMap();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println(wordsInFiles.wordsInNumFiles(1));
+		}
 	}
 }
